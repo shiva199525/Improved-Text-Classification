f8e1a08fc77128784d4aa8767ef4b95638ce5959
Anoja Rajalakshmi <anoja@sfs-wifi-dhcp-10-143-113-181.wifi.sfsu.edu>
2016-11-27 18:50:05 -0800

    Initial Commit

---
+++ Project2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project2
+//
+//  Created by Anoja Rajalakshmi on 27/11/16.
+//  Copyright © 2016 Anoja. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
